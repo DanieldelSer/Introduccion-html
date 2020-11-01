9d6c7cad90c1d74590ce05ee573ed70b8d7114bf
Daniel del Ser <danieldelsermartin@gmail.com>
2020-11-01 13:56:37 +0100

    master

--- Módulo 2/EjercicioConjunto/index.js
+++ Módulo 2/EjercicioConjunto/index.js
@@ -9,147 +9,187 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/almacen", function (req, res) {
-    /* Devuelve un un objeto con tres arrays dentro correspondientes a las secciones */
+  /* Devuelve un un objeto con tres arrays dentro correspondientes a las secciones */
   res.send(almacen);
-})
+});
 
-app.get("/almacen/:departamento", function(req,res){
-    /* Recibe la opcion por params y devuelve el array de objetos del departamento correspondiente */
-    let opcion = req.params.departamento
-    switch (opcion){
-        case "armarios":
-            res.send(almacen.armarios)
-            break;
-        case "mesas":
-            res.send(almacen.mesas)
-            break;
-        case "sillas":
-            res.send(almacen.sillas)
-            break;
-    }
-})
+app.get("/almacen/:departamento", function (req, res) {
+  /* Recibe la opcion por params y devuelve el array de objetos del departamento correspondiente */
+  let opcion = req.params.departamento;
+  switch (opcion) {
+    case "armarios":
+      res.send(almacen.armarios);
+      break;
+    case "mesas":
+      res.send(almacen.mesas);
+      break;
+    case "sillas":
+      res.send(almacen.sillas);
+      break;
+  }
+});
 
-app.post("/almacen", function(req, res){
-    /* Recibe un objeto con los campos del mueble nuevo y el campo adicional opcion. Añade el objeto nuevo al array correspondiente según el valor de opcion y luego devuelve el almacen. En caso de error, devuelve un objeto con el mensaje de error.  */
+app.post("/almacen", function (req, res) {
+  /* Recibe un objeto con los campos del mueble nuevo y el campo adicional opcion. Añade el objeto nuevo al array correspondiente según el valor de opcion y luego devuelve el departamento del almacen seleccionado por opcion. En caso de error, devuelve un objeto con el mensaje de error.  */
+  let opcion = req.body.opcion;
+  let nombre = req.body.nombre.toUpperCase();
+  let descripcion = req.body.descripcion;
+  let img = req.body.img;
+  let precio = req.body.precio;
+  let mueble = {
+    nombre: nombre,
+    descripccion: descripcion,
+    img: img,
+    precio: precio,
+  };
+  console.log(mueble);
+  console.log(opcion);
+  let respuesta;
+  let boolean = false;
+  switch (opcion) {
+    case "armarios":
+      almacen.armarios.push(mueble);
+      respuesta = almacen.armarios;
+      boolean = true;
+      break;
+    case "mesas":
+      almacen.mesas.push(mueble);
+      respuesta = almacen.mesas;
+      boolean = true;
+      break;
+    case "sillas":
+      almacen.sillas.push(mueble);
+      respuesta = almacen.sillas;
+      boolean = true;
+      break;
+  }
+  boolean
+    ? res.send(respuesta)
+    : res.send({
+        error: true,
+        mensaje: "No ha podido añadirse correctamente",
+      });
+});
 
-    let opcion = req.body.opcion
-    let nombre = req.body.nombre
-    let descripcion = req.body.descripcion
-    let img = req.body.img
-    let precio = req.body.precio
-    let mueble = {
-        nombre: nombre,
-        descripccion: descripcion,
-        img: img,
-        precio: precio,
-    }
-    let boolean = false
-    switch (opcion){
-        case "armarios":
-            almacen.armarios.push(mueble)
-            boolean = true
-            break;
-        case "mesas":
-            almacen.mesas.push(mueble)
-            boolean = true
-            break;
-        case "sillas":
-            almacen.sillas.push(mueble)
-            boolean = true
-            break;
-    }
-    boolean 
-    ? res.send(almacen)
-    : res.send({error: true, mensaje: "No ha podido añadirse correctamente"})
-})
+app.put("/almacen", function (req, res) {
+  /* Recibe un objeto con los campos opcion, nombre y precio (además de los valores nuevos para el objeto). Busca el nombre dentro del array del departamento marcado por opcion y si lo encuentra y el precio es el mismo, modifica el valor de los campos del objeto con los nuevos valores recibidos y devuelve el departamento del almacen elegido por opcion. En caso contrario, devuelve error.  */
+  let opcion = req.body.opcion;
+  let nombre = req.body.nombre.toUpperCase();
+  let precio = req.body.precio;
+  let NewNombre = req.body.nombreNuevo.toUpperCase();
+  let NewDescipcion = req.body.descripcion;
+  let NewImg = req.body.img;
+  let NewPrecio = req.body.precioNuevo;
+  let respuesta;
+  let boolean = false;
 
-app.put("/almacen", function(req,res){
-        /* Recibe un objeto con los cambos opcion y nombre (además de los campos del objeto). Busca el nombre dentro del array del departamento marcado por opcion y si lo encuentra, modifica el valor de los campos del objeto con los nuevos valores recibidos y devuelve el almacen. En caso contrario, devuelve error.  */
-    let opcion = req.body.opcion
-    let nombre = req.body.nombre
-    let NewNombre = req.body.nombreNuevo
-    let NewDescipcion = req.body.descripcion
-    let NewImg = req.body.img
-    let NewPrecio = req.body.precio
-    let boolean = false
-    
-    switch (opcion){
-        case "armarios":
-            for(let i = 0; almacen.armarios.length ; i++){
-            if (almacen.armarios[i].nombre.includes(nombre)){
-                almacen.armarios[i].nombre = NewNombre
-                almacen.armarios[i].descripccion = NewDescipcion
-                almacen.armarios[i].img = NewImg
-                almacen.armarios[i].precio = NewPrecio
-                boolean = true
-            }
+  switch (opcion) {
+    case "armarios":
+      for (let i = 0; i < almacen.armarios.length; i++) {
+        if (
+          almacen.armarios[i].nombre.includes(nombre) &&
+          almacen.armarios[i].precio == precio
+        ) {
+          almacen.armarios[i].nombre = NewNombre;
+          almacen.armarios[i].descripccion = NewDescipcion;
+          almacen.armarios[i].img = NewImg;
+          almacen.armarios[i].precio = NewPrecio;
+          respuesta = almacen.armarios;
+          boolean = true;
         }
-            break;
-        case "mesas":
-            for(let i = 0; almacen.mesas.length ; i++){
-            if (almacen.mesas[i].nombre.includes(nombre)){
-                almacen.mesas[i].nombre = NewNombre
-                almacen.mesas[i].descripccion = NewDescipcion
-                almacen.mesas[i].img = NewImg
-                almacen.mesas[i].precio = NewPrecio
-                boolean = true
-            }
+      }
+      break;
+    case "mesas":
+      for (let i = 0; i < almacen.mesas.length; i++) {
+        if (
+          almacen.mesas[i].nombre.includes(nombre) &&
+          almacen.mesas[i].precio == precio
+        ) {
+          almacen.mesas[i].nombre = NewNombre;
+          almacen.mesas[i].descripccion = NewDescipcion;
+          almacen.mesas[i].img = NewImg;
+          almacen.mesas[i].precio = NewPrecio;
+          respuesta = almacen.mesas;
+          boolean = true;
         }
-            break;
-        case "sillas":
-            for(let i = 0; almacen.sillas.length ; i++){
-            if (almacen.sillas[i].nombre.includes(nombre)){
-                almacen.sillas[i].nombre = NewNombre
-                almacen.sillas[i].descripccion = NewDescipcion
-                almacen.sillas[i].img = NewImg
-                almacen.sillas[i].precio = NewPrecio
-                boolean = true
-            }
+      }
+      break;
+    case "sillas":
+      for (let i = 0; i < almacen.sillas.length; i++) {
+        if (
+          almacen.sillas[i].nombre.includes(nombre) &&
+          almacen.sillas[i].precio == precio
+        ) {
+          almacen.sillas[i].nombre = NewNombre;
+          almacen.sillas[i].descripccion = NewDescipcion;
+          almacen.sillas[i].img = NewImg;
+          almacen.sillas[i].precio = NewPrecio;
+          respuesta = almacen.sillas;
+          boolean = true;
         }
-            break;
-            
-    }
-    boolean 
-    ? res.send(almacen)
-    : res.send({error: true, mensaje: "No ha podido modificarse correctamente"})
+      }
+      break;
+  }
+  boolean
+    ? res.send(respuesta)
+    : res.send({
+        error: true,
+        mensaje: "No ha podido modificarse correctamente",
+      });
+});
 
-})
+app.delete("/almacen", function (req, res) {
+  /* Recibe un objeto con los cambos opcion, nombre y precio. Busca el nombre dentro del array del departamento marcado por opcion y si lo encuentra y coincide el precio lo borra y devuelve el departamento del almacen selecionado por opcion. En caso contrario, devuelve error.  */
+  let opcion = req.body.opcion;
+  let nombre = req.body.nombre.toUpperCase();
+  let precio = req.body.precio;
+  let respuesta;
+  let boolean = false;
+  switch (opcion) {
+    case "armarios":
+      for (let i = 0; i < almacen.armarios.length; i++) {
+        if (
+          almacen.armarios[i].nombre.includes(nombre) &&
+          almacen.armarios[i].precio == precio
+        ) {
+          almacen.armarios.splice(i, 1);
+          respuesta = almacen.armarios;
+          boolean = true;
+        }
+      }
+      break;
+    case "mesas":
+      for (let i = 0; i < almacen.mesas.length; i++) {
+        if (
+          almacen.mesas[i].nombre.includes(nombre) &&
+          almacen.mesas[i].precio == precio
+        ) {
+          almacen.mesas.splice(i, 1);
+          respuesta = almacen.mesas;
+          boolean = true;
+        }
+      }
+      break;
+    case "sillas":
+      for (let i = 0; i < almacen.sillas.length; i++) {
+        if (
+          almacen.sillas[i].nombre.includes(nombre) &&
+          almacen.sillas[i].precio == precio
+        ) {
+          almacen.sillas.splice(i, 1);
+          respuesta = almacen.sillas;
+          boolean = true;
+        }
+      }
+      break;
+  }
+  console.log(respuesta)
+  boolean
+    ? res.send(respuesta)
+    : res.send({
+        error: true,
+        mensaje: "No ha podido borrarse correctamente",
+      });
+});
 
-app.delete("/almacen", function(req, res){
-    /* Recibe un objeto con los cambos opcion y nombre. Busca el nombre dentro del array del departamento marcado por opcion y si lo encuentra lo borra y devuelve el almacen. En caso contrario, devuelve error.  */
-    let opcion = req.body.opcion
-    let nombre = req.body.nombre
-    let boolean = false
-    switch (opcion){
-        case "armarios":
-            for(let i = 0; i< almacen.armarios[i].length; i++){
-                if(almacen.armarios[i].nombre.includes(nombre)){
-                    almacen.armarios[i].splice(i,1)
-                    boolean = true
-                }
-            }
-            break;
-        case "mesas":
-            for(let i = 0; i< almacen.mesas[i].length; i++){
-                if(almacen.mesas[i].nombre.includes(nombre)){
-                    almacen.mesas[i].splice(i,1)
-                    boolean = true
-                }
-            }
-            break;
-        case "sillas":
-            for(let i = 0; i< almacen.sillas[i].length; i++){
-                if(almacen.sillas[i].nombre.includes(nombre)){
-                    almacen.sillas[i].splice(i,1)
-                    boolean = true
-                }
-            }
-            break;
-    }
-    boolean 
-    ? res.send(almacen)
-    : res.send({error: true, mensaje: "No ha podido borrarse correctamente"})
-})
-
-
-app.listen(3000)
+app.listen(3000);
--- Módulo 2/EjercicioConjunto/public/index.js
+++ Módulo 2/EjercicioConjunto/public/index.js
@@ -15,6 +15,7 @@ fetch("/almacen")
                  <h3>${datos.armarios[i].nombre}</h3>
                    </h5>${datos.armarios[i].precio}€</h5>
                    <p>${datos.armarios[i].descripccion}</p>
+                   <button onclick="eliminar('${datos.armarios[i].nombre}',${datos.armarios[i].precio})">Eliminar</button>
                 </div>           
             </div>`;
             document.getElementById("mostrarArmarios").innerHTML = impresion;
@@ -30,6 +31,7 @@ fetch("/almacen")
                  <h3>${datos.mesas[i].nombre}</h3>
                    </h5>${datos.mesas[i].precio}€</h5>
                    <p>${datos.mesas[i].descripccion}</p>
+                   <button onclick="eliminar('${datos.mesas[i].nombre}',${datos.mesas[i].precio})">Eliminar</button>
                 </div>           
             </div>`;
             document.getElementById("mostrarMesas").innerHTML = impresion1;
@@ -46,6 +48,7 @@ fetch("/almacen")
                  <h3>${datos.sillas[i].nombre}</h3>
                    </h5>${datos.sillas[i].precio}€</h5>
                    <p>${datos.sillas[i].descripccion}</p>
+                   <button onclick="eliminar('${datos.sillas[i].nombre}',${datos.sillas[i].precio})">Eliminar</button>
                 </div>           
             </div>
          `
@@ -76,6 +79,7 @@ function mostrarDepartamento() {
                  <h3>${datos[i].nombre}</h3>
                    </h5>${datos[i].precio}€</h5>
                    <p>${datos[i].descripccion}</p>
+                   <button onclick="eliminar('${datos[i].nombre}',${datos[i].precio})">Eliminar</button>
                 </div>           
             </div>`;
                 document.getElementById("mostrarArmarios").innerHTML = impresion;
@@ -84,7 +88,6 @@ function mostrarDepartamento() {
 };
 
 function anyadir() {
-    let mostrar = "";
     let nombre = document.getElementById("nombre").value;
     let descripcion = document.getElementById("descripcion").value;
     let img = document.getElementById("img").value;
@@ -95,6 +98,7 @@ function anyadir() {
         descripcion,
         img,
         precio,
+        opcion,
     };
     fetch("/almacen", {
         method: "POST",
@@ -107,6 +111,7 @@ function anyadir() {
             return res.json();
         })
         .then(function (datos) {
+            let mostrar = "";
             document.getElementById("mostrarSillas").innerHTML = "";
             document.getElementById("mostrarMesas").innerHTML = "";
             document.getElementById("mostrarArmarios").innerHTML = "";
@@ -122,7 +127,51 @@ function anyadir() {
                         <p>${datos[i].descripccion}</p>
                      </div>           
                  </div>`;
-                document.getElementById("mostrarArmarios").innerHTML = impresion;
+                document.getElementById("mostrarArmarios").innerHTML = mostrar;
             }
         });
-}
+};
+
+function eliminar(nombre, precio) {
+    let opcion = document.getElementById("opcion").value;
+    console.log('opcion', opcion)
+    if (opcion !== 'armarios' && opcion !== 'sillas' && opcion !== 'mesas') {
+        alert("Falta el tipo de mueble");
+        return;
+    }
+    let mueble = {
+        nombre,
+        precio,
+        opcion,
+    };
+    fetch("/almacen", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(mueble),
+    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (datos) {
+            let mostrar = "";
+            document.getElementById("mostrarSillas").innerHTML = "";
+            document.getElementById("mostrarMesas").innerHTML = "";
+            document.getElementById("mostrarArmarios").innerHTML = "";
+            for (let i = 0; i < datos.length; i++) {
+                mostrar += `
+                 <div>
+                      <div id="img">
+                         <img src="${datos[i].img}" alt="${datos[i].nombre}"/>
+                      </div>
+                      <div id="datos">
+                       <h3>${datos[i].nombre}</h3>
+                         </h5>${datos[i].precio}€</h5>
+                         <p>${datos[i].descripccion}</p>
+                      </div>           
+                  </div>`;
+                document.getElementById("mostrarArmarios").innerHTML = mostrar;
+            }
+        });
+};
