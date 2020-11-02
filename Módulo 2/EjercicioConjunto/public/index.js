77b67cd31dac2da983fca71da67c723222e8fd40
Daniel del Ser <danieldelsermartin@gmail.com>
2020-11-02 02:10:59 +0100

    master

--- Módulo 2/EjercicioConjunto/public/index.js
+++ Módulo 2/EjercicioConjunto/public/index.js
@@ -73,3 +73,44 @@ function mostrarDepartamento() {
             };
         });
 };
+
+function eliminar(nombre, precio) {
+    let opcion = document.getElementById("selectTipo").value;
+    console.log('opcion', opcion)
+    if (opcion !== 'armarios' && opcion !== 'sillas' && opcion !== 'mesas') {
+        alert("Falta el tipo de mueble");
+        return;
+    }
+    let mueble = {
+        nombre,
+        precio,
+        opcion,
+    };
+    fetch("/almacen", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(mueble),
+    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (datos) {
+            let mostrar = "";
+            document.getElementById("mostrarArmarios").innerHTML = "";
+            for (let i = 0; i < datos.length; i++) {
+                mostrar += `
+                <article>
+                    <img src="${datos[i].img}" alt="${datos[i].nombre}"/>
+                    <div class="datos">
+                    <h3>${datos[i].nombre}</h3>
+                    </h5>${datos[i].precio}€</h5>
+                    <p>${datos[i].descripccion}</p>
+                    <button onclick="eliminar('${datos[i].nombre}',${datos[i].precio})" class="inputsBoton">Eliminar</button>        
+                    </div>
+                </article>`;
+                document.getElementById("mostrarArmarios").innerHTML = mostrar;
+            }
+        });
+};
